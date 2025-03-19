89a88501418280f519362a32df214c1c93a4b1e8
SongBeom00 <kjb001023@naver.com>
2025-03-19 10:34:54 +0900

    java-start1

---
+++ src/operator/Operator1.java
@@ -0,0 +1,34 @@
+package operator;
+
+public class Operator1 {
+
+
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("sum = " + sum); // 출력 : a + b = 7
+
+        // 뺄셈
+        int sub = a - b;
+        System.out.println("sub = " + sub); // 출력 : a - b = 3
+
+        // 곱셈
+        int mul = a * b;
+        System.out.println("mul = " + mul); // 출력 : a * b = 10
+
+        // 나눗셈
+        int div = a / b;
+        System.out.println("div = " + div); // 출력 : a / b = 2
+
+        // 나머지
+        int mod = a % b;
+        System.out.println("mod = " + mod); // 출력 : a % b = 1
+
+    }
+
+
+}
